90e3c850ed6ae98748ef1ae3820fe4e46c251a96
Darkorbit17493 <darkorbit17493@gmail.com>
2025-01-30 17:27:20 +0200

    add todo adding UI foundations

--- app/src/main/java/com/example/todoapp/AddTodoActivity.java
+++ app/src/main/java/com/example/todoapp/AddTodoActivity.java
@@ -1,7 +1,7 @@
 package com.example.todoapp;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,20 +9,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class AddTodoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_todo);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
 
-        Intent intent = new Intent(MainActivity.this, AddTodoActivity.class);
-        startActivity(intent);
+    public void onDateButtonClick(View dateButton) {
+
+    }
+
+    public void onTimeButtonClick(View timeButton) {
+
+    }
+
+    public void onCreateButtonClick(View createButton) {
+
     }
 }
